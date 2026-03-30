4a24d287aa91ac188fd8a6577b0cf9f8f7857b2e
tharun52 <thetharun52@gmail.com>
2026-03-30 16:07:32 +0530

    changed to flask

--- backend/backend/rounting.py
+++
@@ -1,6 +0,0 @@
-from channels.routing import route_class
-from notify.consumers import ToDoEventConsumer
-
-channel_routing = [
-    route_class(ToDoEventConsumer, path=r'^/notifications/$'),
-]
--- backend/todo/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from todo.serializers import TodoSerializer
-from todo.models import Todo
-
-
-class TodoView(viewsets.ModelViewSet):
-    serializer_class = TodoSerializer
-    queryset = Todo.objects.all()
