30823a492ee70270f5c0303d5fa2e3fc9892c7cb
tharun52 <thetharun52@gmail.com>
2026-03-30 15:06:17 +0530

    changed setting.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,8 +86,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todo_db',
+        'USER': 'todo_user',
+        'PASSWORD': 'todo123',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
